68a69787b5af7ead25f6801831fee0807b7523d9
isumalatov <isumalatov@gmail.com>
2024-05-16 17:39:10 +0200

    falta poco

--- mcp_bt.cc
+++ mcp_bt.cc
@@ -3,38 +3,68 @@
 #include <fstream>
 #include <vector>
 #include <climits>
+#include <chrono>
+#include <iomanip>
 
 using namespace std;
 
-void mcp_bt(const vector<vector<int>> &matrix, int rows, int cols, int x, int y, int &best_v, int current_v)
+int nvisit = 0;
+int nexplored = 0;
+int nleaf = 0;
+int nunfeasible = 0;
+int nnot_promising = 0;
+
+void mcp_bt(const vector<vector<int>> &matrix, vector<vector<int>> &minCost, vector<vector<bool>> &visited, int rows, int cols, int x, int y, int current_v)
 {
+    nvisit++;
+
+    if (current_v >= minCost[x][y])
+    {
+        nnot_promising++;
+        return;
+    }
+
+    minCost[x][y] = current_v;
+
     if (x == rows - 1 && y == cols - 1)
     {
-        best_v = max(best_v, current_v);
+        nleaf++;
+        return;
     }
     else
     {
-        for (int i = -1; i <= 1; i++)
+        visited[x][y] = true;
+
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {1, 1}, {-1, 1}, {1, -1}};
+        for (auto &dir : directions)
         {
-            for (int j = -1; j <= 1; j++)
+            int newX = x + dir.first;
+            int newY = y + dir.second;
+
+            if (newX >= 0 && newX < rows && newY >= 0 && newY < cols && !visited[newX][newY])
             {
-                if (x + i >= 0 && x + i < rows && y + j >= 0 && y + j < cols)
-                {
-                    mcp_bt(matrix, rows, cols, x + i, y + j, best_v, current_v + matrix[x + i][y + j]);
-                }
+                nexplored++;
+                mcp_bt(matrix, minCost, visited, rows, cols, newX, newY, current_v + matrix[newX][newY]);
+            }
+            else
+            {
+                nunfeasible++;
             }
         }
+
+        visited[x][y] = false;
     }
 }
 
 int mcp_bt(const vector<vector<int>> &matrix, int rows, int cols)
 {
+    vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+    vector<vector<int>> minCost(rows, vector<int>(cols, INT_MAX));
     int x = 0;
     int y = 0;
-    int best_v = INT_MIN;
     int current_v = matrix[x][y];
-    mcp_bt(matrix, rows, cols, x, y, best_v, current_v);
-    return best_v;
+    mcp_bt(matrix, minCost, visited, rows, cols, x, y, current_v);
+    return minCost[rows - 1][cols - 1];
 }
 
 int main(int argc, char *argv[])
@@ -85,8 +115,14 @@ int main(int argc, char *argv[])
         }
     }
 
+    auto start = chrono::high_resolution_clock::now();
     int best_v = mcp_bt(matrix, rows, cols);
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+    double duration_ms = duration.count() / 1000.0;
     cout << best_v << endl;
+    cout << nvisit << " " << nexplored << " " << nleaf << " " << nunfeasible << " " << nnot_promising << endl;
+    cout << fixed << setprecision(3) << duration_ms << endl;
 
     if (p2D)
     {
